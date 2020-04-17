7f90f42b58209851e278df5e9b60ad2c71067678
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2020-04-17 10:46:57 +0200

    Remove /home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: '',
+    component: HomePage
+  }
 ];
 
 @NgModule({
   imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [HomePage]
 })
 export class AppRoutingModule { }
